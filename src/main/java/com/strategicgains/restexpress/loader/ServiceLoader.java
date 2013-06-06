e82558346d2ddc1ce55c5279c7c4853aa664fcfb
brianchen112 <briangxchen@gmail.com>
2013-06-06 20:29:28 +1000

    Add parameter annotation implementation

--- src/main/java/com/strategicgains/restexpress/loader/ServiceLoader.java
+++ src/main/java/com/strategicgains/restexpress/loader/ServiceLoader.java
@@ -1,6 +1,20 @@
 package com.strategicgains.restexpress.loader;
 
+import com.strategicgains.restexpress.RestExpress;
+import com.strategicgains.restexpress.loader.config.AppConfiguration;
+import com.strategicgains.restexpress.loader.config.OperationInfo;
+import com.strategicgains.restexpress.loader.exception.REExceptionMapper;
+import com.strategicgains.restexpress.loader.util.AnnotationUtils;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.jboss.netty.handler.codec.http.HttpMethod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,154 +23,147 @@ import java.util.Map;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.apache.commons.lang.StringUtils;
-import org.jboss.netty.handler.codec.http.HttpMethod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.strategicgains.restexpress.RestExpress;
-import com.strategicgains.restexpress.loader.config.AppConfiguration;
-import com.strategicgains.restexpress.loader.config.OperationInfo;
-import com.strategicgains.restexpress.loader.exception.REExceptionMapper;
-import com.strategicgains.restexpress.loader.util.AnnotationUtils;
 
 /**
  * Load Service Instance and binding the route
- * 
- * @author dev
- * 
+ *
+ * @author BC
  */
 public class ServiceLoader {
-	private static final Logger LOG = LoggerFactory.getLogger(ServiceLoader.class);
+    //~ Static fields/initializers =====================================================================================
 
-	private static Map<String, Object> controllers = new HashMap<String, Object>();
-	private static Map<String, Class<?>> controllerClazzs = new HashMap<String, Class<?>>();
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceLoader.class);
+    private static Map<String, Object> controllers = new HashMap<String, Object>();
+    private static Map<String, Class<?>> controllerClazzs = new HashMap<String, Class<?>>();
 
-	private RestExpress server;
+    //~ Instance fields ================================================================================================
 
-	public void init(AppConfiguration conf, RestExpress server) throws ClassNotFoundException {
-		this.server = server;
-		loadClass(conf.getServiceClasses());
-		routeBind();
-	}
+    private RestExpress server;
 
-	public void loadExMapper(String exceptionMapper) {
-		if (StringUtils.isEmpty(exceptionMapper)){
-			LOG.info("No exception Mapper in the config");
-			return;
-		}
-		REExceptionMapper exMapper = makeClassInstance(exceptionMapper);
-		exMapper.mapExceptions(server);
-	}
+    //~ Methods ========================================================================================================
 
-	public void loadClass(List<String> clazzs) throws ClassNotFoundException {
-		for (String clazz : clazzs) {
-			controllerClazzs.put(clazz, Class.forName(clazz));
-		}
-	}
+    private OperationInfo createOperation(String action, HttpMethod httpMethod, String path, String svcClass,
+        String name, Produces produces, Class<?>[] paraTypes) {
+        OperationInfo op = null;
 
-	private void routeBind() {
-		for (Map.Entry<String, Class<?>> entry : controllerClazzs.entrySet()) {
-			Path rootPath = AnnotationUtils.getClassAnnotation(entry.getValue(), Path.class);
-			String clazzName = entry.getValue().getName();
-			
-			for (Method m : entry.getValue().getMethods()) {
-				Method annotatedMethod = AnnotationUtils.getAnnotatedMethod(m);
-				String httpMethod = AnnotationUtils
-						.getHttpMethodValue(annotatedMethod);
-				Path path = AnnotationUtils.getMethodAnnotation(
-						annotatedMethod, Path.class);
-				
-				Produces produces = AnnotationUtils.getMethodAnnotation(
-						annotatedMethod, Produces.class);
+        if (null == produces) {
+            op = new OperationInfo(action, httpMethod, path, svcClass, name, null, paraTypes);
+        } else {
+            List<String> mediaTypes = getMediaTypes(produces.value());
 
-				if (httpMethod != null || path != null) {
-					String fullPath = rootPath!=null ? rootPath.value() + path.value() : path.value();
-					OperationInfo op = createOperation(m.getName(),
-							new HttpMethod(httpMethod), fullPath,
-							entry.getKey(), clazzName+"." + m.getName(), produces);
-					routeBuild(op);
-				}
-			}
-		}
-	}
-
-	private OperationInfo createOperation(String action,
-			HttpMethod httpMethod, String path, String svcClass, String name, Produces produces) {
-		OperationInfo op = null;
-		if (null == produces){
-			op = new OperationInfo(action, httpMethod, path,
-					svcClass, name, null);
-			
-		}else {
-			List<String> mediaTypes = getMediaTypes(produces.value());
-			
-			if(mediaTypes.size() ==1){
-				String oneType = mediaTypes.get(0);
-				op = new OperationInfo(action, httpMethod, path,
-						svcClass, name, oneType);
-				
-			}else{
-				op = new OperationInfo(action, httpMethod, path,
-						svcClass, name, null);
-			}
-		}
-		
-		return op;
-	}
-	
-	public static List<String> getMediaTypes(String[] values) {
-        List<String> supportedMimeTypes = new ArrayList<String>(values.length);
-        for (int i = 0; i < values.length; i++) {
-            supportedMimeTypes.add(values[i]);    
+            if (mediaTypes.size() == 1) {
+                String oneType = mediaTypes.get(0);
+                op = new OperationInfo(action, httpMethod, path, svcClass, name, oneType, paraTypes);
+            } else {
+                op = new OperationInfo(action, httpMethod, path, svcClass, name, null, paraTypes);
+            }
         }
+
+        return op;
+    }
+
+    public static List<String> getMediaTypes(String[] values) {
+        List<String> supportedMimeTypes = new ArrayList<String>(values.length);
+
+        for (int i = 0; i < values.length; i++) {
+            supportedMimeTypes.add(values[i]);
+        }
+
         return supportedMimeTypes;
     }
 
+    public void init(AppConfiguration conf, RestExpress server)
+        throws ClassNotFoundException {
+        this.server = server;
+        loadClass(conf.getServiceClasses());
+        routeBind();
+    }
 
-	public void routeBuild(OperationInfo op) {
-		LOG.info(op.getPath() +":" + op.getAction() + ":" + op.getHttpMethod() +":" + op.getName());
-		if ("text/plain".equals(op.getProduces())){
-			server.uri(op.getPath(), loadControllerInstance(op.getSvcClass()))
-			.action(op.getAction(), op.getHttpMethod()).name(op.getName()).noSerialization();
-			
-		}else{
-			server.uri(op.getPath(), loadControllerInstance(op.getSvcClass()))
-			.action(op.getAction(), op.getHttpMethod()).name(op.getName());
-		}
-		
-	}
+    public void loadClass(List<String> clazzs) throws ClassNotFoundException {
+        for (String clazz : clazzs) {
+            controllerClazzs.put(clazz, Class.forName(clazz));
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private <T> T loadControllerInstance(String className) {
-		Object obj = controllers.get(className);
-		try {
-			if (null == obj) {
-				obj = controllerClazzs.get(className).newInstance();
-				controllers.put(className, obj);
-			}
-		} catch (InstantiationException e) {
-			LOG.error("Failed to load service instance", e);
-		} catch (IllegalAccessException e) {
-			LOG.error("Failed to load service instance", e);
-		}
-		return(T)obj;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private <T> T makeClassInstance(String clazz){
-		T obj=null;
-		try {
-				obj =(T) Class.forName(clazz).newInstance();
-		} catch (InstantiationException e) {
-			LOG.error("Failed to load service instance", e);
-		} catch (IllegalAccessException e) {
-			LOG.error("Failed to load service instance", e);
-		} catch (ClassNotFoundException e) {
-			LOG.error("Class not found", e);
-		}
-		
-		return obj;
-	}
+    @SuppressWarnings("unchecked")
+    private <T> T loadControllerInstance(String className) {
+        Object obj = controllers.get(className);
 
+        try {
+            if (null == obj) {
+                obj = controllerClazzs.get(className).newInstance();
+                controllers.put(className, obj);
+            }
+        } catch (InstantiationException e) {
+            LOG.error("Failed to load service instance", e);
+        } catch (IllegalAccessException e) {
+            LOG.error("Failed to load service instance", e);
+        }
+
+        return (T) obj;
+    }
+
+    public void loadExMapper(String exceptionMapper) {
+        if (StringUtils.isEmpty(exceptionMapper)) {
+            LOG.info("No exception Mapper in the config");
+
+            return;
+        }
+
+        REExceptionMapper exMapper = makeClassInstance(exceptionMapper);
+        exMapper.mapExceptions(server);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T makeClassInstance(String clazz) {
+        T obj = null;
+
+        try {
+            obj = (T) Class.forName(clazz).newInstance();
+        } catch (InstantiationException e) {
+            LOG.error("Failed to load service instance", e);
+        } catch (IllegalAccessException e) {
+            LOG.error("Failed to load service instance", e);
+        } catch (ClassNotFoundException e) {
+            LOG.error("Class not found", e);
+        }
+
+        return obj;
+    }
+
+    private void routeBind() {
+        for (Map.Entry<String, Class<?>> entry : controllerClazzs.entrySet()) {
+            Path rootPath = AnnotationUtils.getClassAnnotation(entry.getValue(), Path.class);
+            String clazzName = entry.getValue().getName();
+
+            for (Method m : entry.getValue().getMethods()) {
+                Method annotatedMethod = AnnotationUtils.getAnnotatedMethod(m);
+                String httpMethod = AnnotationUtils.getHttpMethodValue(annotatedMethod);
+                Path path = AnnotationUtils.getMethodAnnotation(annotatedMethod, Path.class);
+
+                Produces produces = AnnotationUtils.getMethodAnnotation(annotatedMethod, Produces.class);
+
+                Class<?>[] paraTypes = m.getParameterTypes();
+
+                if ((httpMethod != null) || (path != null)) {
+                    String fullPath = (rootPath != null) ? (rootPath.value() + path.value()) : path.value();
+                    OperationInfo op = createOperation(m.getName(), new HttpMethod(httpMethod), fullPath,
+                            entry.getKey(), clazzName + "." + m.getName(), produces, paraTypes);
+                    routeBuild(op);
+                }
+            }
+        }
+    }
+
+    public void routeBuild(OperationInfo op) {
+        LOG.info(op.getPath() + ":" + op.getAction() + ":" + op.getHttpMethod() + ":" + op.getName());
+
+        if ("text/plain".equals(op.getProduces())) {
+            server.uri(op.getPath(), loadControllerInstance(op.getSvcClass()))
+                  .action(op.getAction(), op.getHttpMethod(), op.getParaClasses()).name(op.getName()).noSerialization();
+        } else {
+            server.uri(op.getPath(), loadControllerInstance(op.getSvcClass()))
+                  .action(op.getAction(), op.getHttpMethod(), op.getParaClasses()).name(op.getName());
+        }
+    }
 }
--- src/main/java/com/strategicgains/restexpress/loader/config/OperationInfo.java
+++ src/main/java/com/strategicgains/restexpress/loader/config/OperationInfo.java
@@ -2,117 +2,167 @@ package com.strategicgains.restexpress.loader.config;
 
 import org.jboss.netty.handler.codec.http.HttpMethod;
 
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision$
+ */
 public class OperationInfo {
-	String action;
-	HttpMethod httpMethod;
-	String path;
-	String svcClass;
-	String name;
-	String produces;
+    //~ Instance fields ================================================================================================
 
-	/**
-	 * @return the produces
-	 */
-	public String getProduces() {
-		return produces;
-	}
+    HttpMethod httpMethod;
+    String action;
+    String name;
+    String path;
+    String produces;
+    String svcClass;
+    Class<?>[] paraClasses;
 
-	/**
-	 * @param produces
-	 *            the produces to set
-	 */
-	public void setProduces(String produces) {
-		this.produces = produces;
-	}
+    //~ Constructors ===================================================================================================
 
-	public OperationInfo() {
-		super();
-	}
+    public OperationInfo() {
+        super();
+    }
 
-	public OperationInfo(String action, HttpMethod httpMethod, String path,
-			String svcClass, String name, String produces) {
-		super();
-		this.action = action;
-		this.httpMethod = httpMethod;
-		this.path = path;
-		this.svcClass = svcClass;
-		this.name = name;
-		this.produces = produces;
-	}
+    public OperationInfo(String action, HttpMethod httpMethod, String path, String svcClass, String name,
+        String produces, Class<?>[] paraClasses) {
+        super();
+        this.action = action;
+        this.httpMethod = httpMethod;
+        this.path = path;
+        this.svcClass = svcClass;
+        this.name = name;
+        this.produces = produces;
+        this.paraClasses = paraClasses;
+    }
 
-	/**
-	 * @return the action
-	 */
-	public String getAction() {
-		return action;
-	}
+    //~ Methods ========================================================================================================
 
-	/**
-	 * @param action
-	 *            the action to set
-	 */
-	public void setAction(String action) {
-		this.action = action;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the action
+     */
+    public String getAction() {
+        return action;
+    }
 
-	/**
-	 * @return the httpMethod
-	 */
-	public HttpMethod getHttpMethod() {
-		return httpMethod;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the httpMethod
+     */
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
 
-	/**
-	 * @param httpMethod
-	 *            the httpMethod to set
-	 */
-	public void setHttpMethod(HttpMethod httpMethod) {
-		this.httpMethod = httpMethod;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @return the path
-	 */
-	public String getPath() {
-		return path;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the paraClasses
+     */
+    public Class<?>[] getParaClasses() {
+        return paraClasses;
+    }
 
-	/**
-	 * @param path
-	 *            the path to set
-	 */
-	public void setPath(String path) {
-		this.path = path;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the path
+     */
+    public String getPath() {
+        return path;
+    }
 
-	/**
-	 * @return the svcClass
-	 */
-	public String getSvcClass() {
-		return svcClass;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the produces
+     */
+    public String getProduces() {
+        return produces;
+    }
 
-	/**
-	 * @param svcClass
-	 *            the svcClass to set
-	 */
-	public void setSvcClass(String svcClass) {
-		this.svcClass = svcClass;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @return the svcClass
+     */
+    public String getSvcClass() {
+        return svcClass;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @param action the action to set
+     */
+    public void setAction(String action) {
+        this.action = action;
+    }
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * DOCUMENT ME!
+     *
+     * @param httpMethod the httpMethod to set
+     */
+    public void setHttpMethod(HttpMethod httpMethod) {
+        this.httpMethod = httpMethod;
+    }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param paraClasses the paraClasses to set
+     */
+    public void setParaClasses(Class<?>[] paraClasses) {
+        this.paraClasses = paraClasses;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param path the path to set
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param produces the produces to set
+     */
+    public void setProduces(String produces) {
+        this.produces = produces;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param svcClass the svcClass to set
+     */
+    public void setSvcClass(String svcClass) {
+        this.svcClass = svcClass;
+    }
 }
